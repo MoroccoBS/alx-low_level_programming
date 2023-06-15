bd859d2e2216256efe96390b4a4e7ce9a4f0a29c
MrBs <Chouaibwidadipur@gmail.com>
2023-06-15 16:19:01 +0100

    Revert "Update 5-printf.c"
    This reverts commit 325cda0917ef944292506d294eebe5f641de01bf.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-printf("with proper sorry I changed This, but the outcome is a piece of art,\n");
-return (1);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
