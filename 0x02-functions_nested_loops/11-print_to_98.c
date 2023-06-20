d4ee0a18974d8e98916538d0adbc37bbaee6cf16
MrBs <Chouaibwidadipur@gmail.com>
2023-06-20 17:29:29 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,26 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 98)
 	{
-
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-	}
-	else
+	} else if (n >= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%d", i);
-			if (i != 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	_putchar('\n');
 }
+
