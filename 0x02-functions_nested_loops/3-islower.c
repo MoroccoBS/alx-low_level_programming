23dca3a090debb0a4f4377774cf6125484e0b0e8
MrBs <Chouaibwidadipur@gmail.com>
2023-06-20 15:47:39 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - cheks for lowercase character
- *
+ * @c: the letter tha will be pased to the function to check
  * Return: (1) succes (0) otherwise
  */
 int _islower(int c)
