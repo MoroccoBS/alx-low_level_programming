2a9a505ae7833d4df08264146909c7d1caa45c65
MrBs <Chouaibwidadipur@gmail.com>
2023-06-20 14:39:24 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print Putchar
+ *
+ * Return: Always (0) success 
+ */
+int main(void)
+{
+    custom_putchar('A');
+    return (0);
+}
+
+int custom_putchar(char c)
+{
+    return (putchar(c));
+}
