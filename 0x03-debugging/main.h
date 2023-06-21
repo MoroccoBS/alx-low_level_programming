f169a12b3524c6230467a90b75cb4bee6d8384ae
MrBs <Chouaibwidadipur@gmail.com>
2023-06-21 12:01:45 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+int latgest_number(int a, int b, int c);
+void print_remaining_days(int month, int day);
+#endif
