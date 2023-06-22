1903788646e30127954bb9a5f91cbf2283ee94ce
MrBs <Chouaibwidadipur@gmail.com>
2023-06-22 17:36:02 +0100

    Added print_most_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ * Return: void
  */
-
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
