7d7c0af24982a4d5320a8f6eaf46b4f741d4c4f1
MrBs <Chouaibwidadipur@gmail.com>
2023-06-26 13:02:21 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @str: string to be printed 
+ * @str: string to be printed
  * Return: void
  */
 void _puts(char *str)
