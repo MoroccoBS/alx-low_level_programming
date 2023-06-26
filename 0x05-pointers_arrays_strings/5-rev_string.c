616c8540a34047eefac5a409c00af86ab8e91be9
MrBs <Chouaibwidadipur@gmail.com>
2023-06-26 13:28:06 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    int len = 0, i;
-    char temp;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+    	int len = 0, i;
+    	char temp;
 
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+    	while (s[len] != '\0')
+    	{
+        	len++;
+    	}
+
+    	for (i = 0; i < len / 2; i++)
+    	{
+        	temp = s[i];
+        	s[i] = s[len - i - 1];
+        	s[len - i - 1] = temp;
+    	}
 }
-
