587441033704cff66fcc64a60481b980be34c20b
MrBs <Chouaibwidadipur@gmail.com>
2023-06-26 14:43:25 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed
+ * @dest: buffer
+ * @src: string to be pointed by src
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return(dest_start);
+}
