145dab982a85f8fe6c63fa6e5cee5279849cfb81
MrBs <Chouaibwidadipur@gmail.com>
2023-06-26 13:53:56 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i] != '\n')
 	{
 		putchar(str[i]);
 		i += 2;
 	}
-	printf("\n");
+	putchar('\n');
 }
