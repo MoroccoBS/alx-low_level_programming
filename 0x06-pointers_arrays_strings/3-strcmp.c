d258f6d94bc5e49c4a7f7ba66a90fab8c4be8466
MrBs <Chouaibwidadipur@gmail.com>
2023-06-30 13:41:28 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,4 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i;
+	int dif = 0;
 
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			dif = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (dif);
+}
