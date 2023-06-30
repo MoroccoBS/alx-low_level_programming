cfc9e74c7c5de3cfa78c68ef1364225544a80fa3
MrBs <Chouaibwidadipur@gmail.com>
2023-06-30 22:12:51 +0100

    Update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,59 +1,49 @@
 #include "main.h"
+
 /**
- * print_buffer - a function that prints a buffer.
- * @b: buffer pointer
- * @size: buffer size
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
+    int o, j, i;
+
+    o = 0;
+
     if (size <= 0)
     {
         printf("\n");
         return;
     }
-
-    for (int i = 0; i < size; i += 10)
+    while (o < size)
     {
-        printf("%08x ", i);
-
-        for (int j = i; j < i + 10; j += 2)
+        j = size - o < 10 ? size - o : 10;
+        printf("%08x: ", o);
+        for (i = 0; i < 10; i++)
         {
-            if (j < size)
+            if (i < j)
+                printf("%02x", *(b + o + i));
+            else
+                printf("  ");
+            if (i % 2)
             {
-                printf("%02x", b[j] & 0xFF);
-
-                if (j + 1 < size)
-                {
-                    printf("%02x", b[j + 1] & 0xFF);
-                }
-                else
-                {
-                    printf("  ");
-                }
-
                 printf(" ");
             }
-            else
-            {
-                printf("      ");
-            }
         }
-
-        printf(" ");
-
-        for (int j = i; j < i + 10 && j < size; j++)
+        for (i = 0; i < j; i++)
         {
-            if (b[j] >= 32 && b[j] <= 126)
-            {
-                printf("%c", b[j]);
-            }
-            else
-            {
-                printf(".");
-            }
-        }
+            int c = *(b + o + i);
 
+            if (c < 32 || c > 132)
+            {
+                c = '.';
+            }
+            printf("%c", c);
+        }
         printf("\n");
+        o += 10;
     }
 }
