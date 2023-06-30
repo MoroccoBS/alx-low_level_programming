722d9954978674f4dd463651e83d5f86452f8d69
MrBs <Chouaibwidadipur@gmail.com>
2023-06-30 21:57:00 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,20 +6,20 @@
  */
 char *rot13(char *str)
 {
-    int i, j;
-    char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char dataRot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char am[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; j < 52; j++)
-        {
-            if (str[i] == data1[j])
-            {
-                str[i] = dataRot[j];
-                break;
-            }
-        }
-    }
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == am[j])
+			{
+				str[i] = rot[i];
+				break;
+			}
+		}
+	}
+	return (str);
 }
