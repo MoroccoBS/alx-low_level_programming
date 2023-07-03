42a2d084f9c4089f2756bff3dc0e77f17e977f6b
MrBs <Chouaibwidadipur@gmail.com>
2023-07-03 14:27:00 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,33 +6,33 @@
  */
 char *_strchr(char *s, char c)
 {
-	int len = 0;
-	int i, n;
-	int *j = NULL;
+    int len = 0;
+    int i, n;
+    char *j = NULL;
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		if (s[i] == c)
-		{
-			n = i;
-			j = &s[n];
-			break;
-		}
-	}
-	if (j != NULL)
-	{
-		if (len - n >= 3)
-		{
-			return (j);
-		}
-		else
-		{
-			return (NULL);
-		}
-	}
-	return (NULL);
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    for (i = 0; i < len; i++)
+    {
+        if (s[i] == c)
+        {
+            n = i;
+            j = &s[n];
+            break;
+        }
+    }
+    if (j != NULL)
+    {
+        if (len - n >= 3)
+        {
+            return (j);
+        }
+        else
+        {
+            return (NULL);
+        }
+    }
+    return (NULL);
 }
