2659c9378cd421c452cf09e5360da12e97741895
MrBs <Chouaibwidadipur@gmail.com>
2023-07-07 23:12:28 +0100

    Started 0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Main function
+ * @argc: number of command-line arguments passed
+ * @arcgv: array of strings
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
