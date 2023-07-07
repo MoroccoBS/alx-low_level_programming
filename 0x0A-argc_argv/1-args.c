dc2cfd96c2e9f431aaceb50118eec4d649d44097
MrBs <Chouaibwidadipur@gmail.com>
2023-07-07 23:21:55 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Main function
+ * @argc: number of command-line arguments passed
+ * @argv: array of strings
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
