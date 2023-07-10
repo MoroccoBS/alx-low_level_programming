85126e8b91adfeb50bfbda2618214796076df2b6
MrBs <Chouaibwidadipur@gmail.com>
2023-07-10 20:00:57 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,41 +7,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int i = 0, j = 0;
-	int s1_len = 0, s2_len = 0;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-	{
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	}
-	if (s1 != NULL)
-		while (s1[i] != '\0')
+
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
 		{
-			s1_len++;
+			s3[i] = s1[i];
 			i++;
 		}
-	if (s2 != NULL)
-		while (s2[j] != '\0')
+	}
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
 		{
-			s2_len++;
+			s3[i] = s2[j];
+			i++;
 			j++;
 		}
-	ptr = malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (ptr == NULL)
-	{
-		return (NULL);
 	}
-	if (s1 != NULL)
-		for (i = 0; i < s1_len; i++)
-		{
-			ptr[i] = s1[i];
-		}
-	if (s2 != NULL)
-		for (j = 0; j < s2_len; j++)
-		{
-			ptr[i + j] = s2[j];
-		}
-	ptr[i + j] = '\0';
-	return (ptr);
+	s3[i] = '\0';
+
+	return (s3);
 }
