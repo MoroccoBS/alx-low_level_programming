a1236206de1ab1785f27290d2517d0664945e49c
MrBs <Chouaibwidadipur@gmail.com>
2023-07-13 12:24:26 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,12 +33,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     ptr[i] = '\0';
     return (ptr);
 }
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
-}
