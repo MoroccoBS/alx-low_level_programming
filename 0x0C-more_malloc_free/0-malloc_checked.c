a5db1b76c7afc1c360c6d4d4c2b75017d442c218
MrBs <Chouaibwidadipur@gmail.com>
2023-07-13 12:22:48 +0100

    Added 1-string_nconcat.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * malloc_checked - function that allocates memory
  * @b: size
@@ -7,10 +8,12 @@
 void *malloc_checked(unsigned int b)
 {
     void *ptr;
+
     ptr = malloc(b);
     if (ptr == NULL)
     {
         exit(98);
     }
+
     return (ptr);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: pointer to the resulting string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *ptr;
+    unsigned int i;
+    unsigned int j;
+    unsigned int len1 = 0;
+    unsigned int len2 = 0;
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+    for (i = 0; s1[i] != '\0'; i++)
+        len1++;
+    for (j = 0; s2[j] != '\0'; j++)
+        len2++;
+    if (n > len2)
+        n = len2;
+    ptr = malloc(len1 + n + 1);
+    if (ptr == NULL)
+        return (NULL);
+    for (i = 0; i < len1; i++)
+        ptr[i] = s1[i];
+    for (j = 0; j < n; j++)
+        ptr[i++] = s2[j];
+    ptr[i] = '\0';
+    return (ptr);
+}
