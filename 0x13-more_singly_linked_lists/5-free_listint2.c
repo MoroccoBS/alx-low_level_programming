32486e71f0c55e5c1837098d4f3ab67d9d9b2d9c
MrBs <Chouaibwidadipur@gmail.com>
2023-08-01 01:34:33 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,27 @@ void free_listint2(listint_t **head)
     }
     while (head != NULL)
     {
-        temp = head;
+        *temp = **head;
         *head = (*head)->next;
         free(temp);
     }
+}
+
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    return (0);
 }
