7bbff28432ea68734b29a1e671458a39ceba8079
MrBs <Chouaibwidadipur@gmail.com>
2023-08-01 15:40:16 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,11 @@
 
 size_t listint_len(const listint_t *h)
 {
-    unsigned int i = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        i++;
-    }
-    return (i);
+	unsigned int i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
