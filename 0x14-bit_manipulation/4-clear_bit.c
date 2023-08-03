99618844184d7e89dc3e38b240c4d98ccf04419b
MrBs <Chouaibwidadipur@gmail.com>
2023-08-03 14:22:52 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,13 +8,12 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = ~(1UL << index);
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-
-	unsigned long int mask = ~(1UL << index);
 	*n = *n & mask;
-
 	return (1);
 }
