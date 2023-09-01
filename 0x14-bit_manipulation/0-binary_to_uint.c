85dc624dc93b69f4cd46a66c308dd8c9ed0df7f4
MrBs <Chouaibwidadipur@gmail.com>
2023-09-01 02:05:46 +0100

    Updated 0-file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int dec = 0; /* dec is the variable to hold the decimal format */
 
 	if (b == NULL)
-	{
 		return (0);
-	}
-	while (*b != '\0')
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
 
-		result = (result << 1) | (*b - '0');
+	while (*b)
+	{
+		if (*b == '1')
+			dec = (dec << 1) | 1;
+		else if (*b == '0')
+			dec <<= 1;
+		else
+			return (0);
 		b++;
 	}
 
-	return (result);
+	return (dec);
 }
