2f464be712167789da4be51607b12a8af4d50824
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-12 16:30:17 +0100

    Updated 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,6 +10,10 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *current = *h;
+	unsigned int count = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (h == NULL)
 	{
 		return (NULL);
@@ -33,9 +37,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	dlistint_t *current = *h;
-	unsigned int count = 0;
-
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
@@ -47,7 +48,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -62,4 +62,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current->next = new_node;
 
 	return (new_node);
+}
+
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
+	print_dlistint(head);
+	printf("-----------------\n");
+	insert_dnodeint_at_index(&head, 5, 4096);
+	print_dlistint(head);
+	free_dlistint(head);
+	head = NULL;
+	return (EXIT_SUCCESS);
 }
