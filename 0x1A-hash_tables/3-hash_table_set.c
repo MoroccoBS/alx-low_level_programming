55f2323c0a3cb01992f4ba27f305b15544e7e004
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-03 17:04:06 +0100

    Updated 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,35 +10,37 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long index, size;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+	if (index >= size)
+	{
+		return (0);
+	}
+
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
+
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (0);
+	}
 
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return 0;
-	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
