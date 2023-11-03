a386c6d136586c951fb7f683e63ebf3200e92ea7
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-03 17:07:46 +0100

    Updated 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,14 +11,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL)
-		return (NULL);
+		return NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-		return (NULL);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return strdup(node->value);
+		node = node->next;
+	}
 
-	return (ht->array[index]->value);
+	return NULL;
 }
